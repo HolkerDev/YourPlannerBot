7d3c4887d622d7d49ef99a056cd8224f3e982221
Holker <nick.penkovskiy@gmail.com>
2019-11-02 15:38:01 +0200

    Add debug logging

--- main.py
+++ main.py
@@ -16,9 +16,8 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
